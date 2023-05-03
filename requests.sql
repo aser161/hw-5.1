6ca7769b9c7b0fcf0f5177b5e7c41ea4cccae2e1
aser161 <aser161@yandex.ru>
2023-05-03 23:32:23 +0300

    Выборка данных и вложенные запросы

--- requests.sql
+++ requests.sql
@@ -1,29 +1,50 @@
 CREATE TABLE employee
-    (
+(
     id         BIGSERIAL   NOT NULL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name  VARCHAR(50) NOT NULL,
     gender     VARCHAR(6)  NOT NULL,
     age        INT         NOT NULL
-    );
+);
 
 SELECT * FROM employee;
 
 INSERT INTO employee (first_name, last_name, gender, age)
-VALUES ('Bob', 'Green', 'male', 25);
-VALUES ('John', 'Brown', 'male', 30);
-VALUES ('Jana', 'White', 'female', 28);
+VALUES ('Bob', 'Green', 'male', 25),
+       ('John', 'Brown', 'male', 30),
+       ('Jana', 'White', 'female', 28),
+       ('Kathy', 'Rid', 'female', 22),
+       ('Mary', 'Kim', 'female', 52);
 
 SELECT * FROM employee;
 
-UPDATE employee SET first_name = 'Kathy',
-                    last_name = 'Rid',
-                    gender = 'female',
-                    age = 22
-                    WHERE id = 1;
+SELECT first_name AS Имя,
+       last_name AS Фамилия
+FROM employee;
 
-SELECT * FROM employee;
+SELECT * FROM employee WHERE age<30 OR age>50;
 
-DELETE FROM employee WHERE id = 3;
+SELECT * FROM employee WHERE age>30 AND age<50;
 
-SELECT * FROM employee;
+SELECT * FROM employee
+ORDER BY last_name DESC, first_name;
+
+SELECT * FROM employee WHERE employee.last_name LIKE '_ _ _ _ %';
+
+UPDATE employee SET first_name = 'Bob' WHERE id = 2;
+UPDATE employee SET first_name = 'Kathy' WHERE id = 5;
+
+SELECT first_name AS имя, SUM(age) AS суммарный_возраст
+FROM employee GROUP BY first_name;
+
+SELECT * FROM employee
+WHERE age = (SELECT MIN(age)
+             FROM employee);
+
+SELECT * FROM employee
+WHERE age = (SELECT MAX(age)
+             FROM employee);
+
+
+
+
